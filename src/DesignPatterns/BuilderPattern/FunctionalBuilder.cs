3324339c6813604c59db510b1d1cab4b517391f8
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-07-18 16:33:43 +0200

    Przeniesienie do innych plików

--- src/DesignPatterns/BuilderPattern/FunctionalBuilder.cs
+++ src/DesignPatterns/BuilderPattern/FunctionalBuilder.cs
@@ -25,30 +25,4 @@ namespace BuilderPattern
         public TSubject Build() => actions.Aggregate(new TSubject(), (p, f) => f(p));
 
     }
-
-    public class SalesReportFunctionalBuilder : FunctionalBuilder<SalesReport, SalesReportFunctionalBuilder>
-    {
-        private IEnumerable<Order> orders;
-
-        public SalesReportFunctionalBuilder(IEnumerable<Order> orders)
-        {
-            this.orders = orders;
-        }
-
-        public SalesReportFunctionalBuilder AddHeader(string title) => Do(p => p.Title = title);
-
-        public SalesReportFunctionalBuilder AddGenderSection() => Do(p => AddGenderSection(p));
-
-        private SalesReportFunctionalBuilder AddGenderSection(SalesReport salesReport)
-        {
-            salesReport.GenderDetails = orders
-               .GroupBy(o => o.Customer.Gender)
-               .Select(g => new GenderReportDetail(
-                           g.Key,
-                           g.Sum(x => x.Details.Sum(d => d.Quantity)),
-                           g.Sum(x => x.Details.Sum(d => d.LineTotal))));
-
-            return this;
-        }
-    }
 }
