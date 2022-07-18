e538b9aed58c206c12ecfeaef7ef4cc3cf30554a
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-07-18 12:12:06 +0200

    Refaktoring

--- src/DesignPatterns/AbstractFactoryPattern/IconFactory.cs
+++ src/DesignPatterns/AbstractFactoryPattern/IconFactory.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,16 +18,5 @@ namespace AbstractFactoryPattern
                 default: return string.Empty;
             }
         }
-    }
-
-    public class PaymentTypeFactory
-    {
-        public static PaymentType Create(string input) => input switch
-        {
-            "G" => PaymentType.Cash,
-            "K" => PaymentType.CreditCard,
-            "P" => PaymentType.BankTransfer,
-            _ => throw new NotSupportedException(),
-        };
     }
 }
---
+++ src/DesignPatterns/AbstractFactoryPattern/PaymentTypeFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AbstractFactoryPattern
+{
+    public class PaymentTypeFactory
+    {
+        public static PaymentType Create(string input) => input switch
+        {
+            "G" => PaymentType.Cash,
+            "K" => PaymentType.CreditCard,
+            "P" => PaymentType.BankTransfer,
+            _ => throw new NotSupportedException(),
+        };
+    }
+}
