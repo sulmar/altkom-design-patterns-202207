f32bcbf4898207b89764e8aac95a17b31a56e31e
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-07-19 15:22:07 +0200

    Dodanie przykładu wzorca metoda szablonowa

--- src/DesignPatterns/TemplateMethodPattern/GenderPercentageOrderCalculator.cs
+++ src/DesignPatterns/TemplateMethodPattern/GenderPercentageOrderCalculator.cs
@@ -1,7 +1,7 @@
 ﻿namespace TemplateMethodPattern
 {
     // Gender - 20% upustu dla kobiet
-    public class GenderPercentageOrderCalculator
+    public class GenderPercentageOrderCalculator : TemplateOrderCalculator
     {
         private readonly Gender gender;
 
@@ -13,14 +13,36 @@
             this.percentage = percentage;
         }
 
+        public override bool CanDiscount(Order order) => order.Customer.Gender == gender;
+        public override decimal Discount(Order order) => order.Amount * percentage;
+    }
+
+    
+
+    // Szablon metody (Template Method)
+    public abstract class TemplateOrderCalculator
+    {
+        public abstract bool CanDiscount(Order order);
+        public abstract decimal Discount(Order order);
+        public virtual decimal NoDiscount()
+        {
+            return decimal.Zero;
+        }
+
         public decimal CalculateDiscount(Order order)
         {
-            if (order.Customer.Gender == gender)
+
+
+            if (CanDiscount(order))            // warunek (predykat)
             {
-                return order.Amount * percentage;
+                return Discount(order);        // obliczenie upustu
             }
             else
-                return 0;
+                return NoDiscount();                // brak upustu
         }
     }
+
+   
+
+
 }
--- src/DesignPatterns/TemplateMethodPattern/HappyHoursOrderCalculator.cs
+++ src/DesignPatterns/TemplateMethodPattern/HappyHoursOrderCalculator.cs
@@ -1,29 +1,25 @@
-﻿namespace TemplateMethodPattern
+﻿using System;
+
+namespace TemplateMethodPattern
 {
-    // Happy Hours - 10% upustu w godzinach od 9 do 15
-    public class HappyHoursPercentageOrderCalculator
+    // Happy Hours - 10% upustu w godzinach od 8:30 do 15
+    public class HappyHoursPercentageOrderCalculator : TemplateOrderCalculator
     {
-        private readonly int from;
-        private readonly int to;
+        private readonly TimeSpan from;
+        private readonly TimeSpan to;
 
         private readonly decimal percentage;
 
-        public HappyHoursPercentageOrderCalculator(int from, int to, decimal percentage)
+        public HappyHoursPercentageOrderCalculator(TimeSpan from, TimeSpan to, decimal percentage)
         {
             this.from = from;
             this.to = to;
             this.percentage = percentage;            
         }
 
-        public decimal CalculateDiscount(Order order)
-        {
-            if (order.OrderDate.Hour >= from && order.OrderDate.Hour < to)
-            {
-                return order.Amount * percentage;
-            }
-            else
-                return 0;
-        }
+        public override bool CanDiscount(Order order) => order.OrderDate.TimeOfDay >= from && order.OrderDate.TimeOfDay < to;
+        public override decimal Discount(Order order) => order.Amount * percentage;
+
     }
 
 }
