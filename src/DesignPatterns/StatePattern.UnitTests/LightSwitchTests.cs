60d05e69203915601bb0049915c0eaf80c848ae6
Marcin Sulecki <marcin.sulecki@sulmar.pl>
2022-07-21 19:06:26 +0200

    Dodanie przykładu wzorca stanu

---
+++ src/DesignPatterns/StatePattern.UnitTests/LightSwitchTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatePattern.Solution;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern.UnitTests
+{
+    [TestClass]
+    public class LightSwitchTests
+    {
+        [TestMethod]
+        public void Init_WhenCalled_ShouldStateIsOff()
+        {
+            // Arrange
+
+            // Act
+            LightSwitch lightSwitch = new LightSwitch();
+
+            // Assert
+            Assert.IsInstanceOfType(lightSwitch.State, typeof(Off));
+
+        }
+
+        [TestMethod]
+        public void Push_Once_ShouldStateIsOn()
+        {
+            // Arrange
+            LightSwitch lightSwitch = new LightSwitch();
+
+            // Act
+            lightSwitch.Push();
+
+            // Assert
+            Assert.IsInstanceOfType(lightSwitch.State, typeof(On));
+        }
+
+        [TestMethod]
+        //  [ExpectedException(typeof(InvalidOperationException))]
+        public void PushDown_Twice_ShouldStateIsOff()
+        {
+            // Arrange
+            LightSwitch lightSwitch = new LightSwitch();
+
+            // Act
+            lightSwitch.Push();
+            lightSwitch.Push();
+
+            // Assert
+            Assert.IsInstanceOfType(lightSwitch.State, typeof(Off));
+        }
+    }
+}
---
+++ src/DesignPatterns/StatePattern/LightSwitch.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatePattern.Problem
+{
+    public class LightSwitch
+    {
+        public LightSwitchState State { get; set; }
+
+        public void Push()
+        {
+            if (State == LightSwitchState.Off)
+            {
+                Console.WriteLine("włącz przekaźnik");
+
+                State = LightSwitchState.On;
+                return;
+            }
+
+            if (State == LightSwitchState.On)
+            {
+                Console.WriteLine("wyłącz przekaźnik");
+
+                State = LightSwitchState.Off;
+                return;
+            }
+        }
+    }
+
+    public enum LightSwitchState
+    {
+        On,
+        Off
+    }
+}
+
+namespace StatePattern.Solution
+{
+    // Abstract State
+    public interface LightSwitchState
+    {
+        // Handle
+        void Push(LightSwitch lightSwitch);
+    }
+
+    // Concrete State
+    public class On : LightSwitchState
+    {
+        public void Push(LightSwitch lightSwitch)
+        {
+            Console.WriteLine("wyłącz przekaźnik");
+
+            lightSwitch.SetState(new Off());            
+        }
+    }
+
+    // Concrete State
+    public class Off : LightSwitchState
+    {
+        public void Push(LightSwitch lightSwitch)
+        {
+            Console.WriteLine("załącz przekaźnik");
+
+            lightSwitch.SetState(new On());
+        }
+    }
+
+    // Context
+    public class LightSwitch
+    {
+        public LightSwitchState State { get; private set; }
+
+        public LightSwitch()
+        {
+            State = new Off();
+        }
+
+        public void SetState(LightSwitchState state) => this.State = state;
+
+        // Request
+        public void Push() => State.Push(this);
+    }
+}
